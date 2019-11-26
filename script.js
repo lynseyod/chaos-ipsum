5d3f1437c5f078f68ace05d3c16cf3852246422e
lynseyod <lyns.odonnell@gmail.com>
2019-11-26 00:44:03 -0500

    v smol syntax changes :mouse::hamster:

--- script.js
+++ script.js
@@ -2,246 +2,256 @@ const SAFIISAWESOME = {};
 
 SAFIISAWESOME.quotes = [
   {
-    quote: "Everything is nothing, and the universe is chaos",
+    quote: `Everything is nothing, and the universe is chaos`,
     wordCount: 8
   },
   {
-    quote: "Deborah",
+    quote: `<em>Deborah!</em>`,
     wordCount: 1,
-    append: true
+    append: true,
+    punctuation: false
   },
   {
-    quote: "My favourite coffee shop is",
-    wordCount: 4
+    quote: `My favourite coffee shop is`,
+    wordCount: 4,
+    punctuation: false
   },
   {
-    quote: "I wanted to take a sip of my coffee",
+    quote: `I wanted to take a sip of my coffee`,
     wordCount: 9
   },
   {
-    quote: "This is all made up and none of it matters",
+    quote: `This is all made up and none of it matters`,
     wordCount: 10
   },
   {
-    quote: "If you try to find resolution outside self you'll be miserable and lonely",
+    quote: `If you try to find resolution outside self you'll be miserable and lonely`,
     wordCount: 13
   },
   {
-    quote: "We are all fundamentally alone",
+    quote: `We are all fundamentally alone`,
     wordCount: 5
   },
   {
-    quote: "Everything beyond the self is chaos",
+    quote: `Everything beyond the self is chaos`,
     wordCount: 6
   },
   {
-    quote: "SAFIISHANDSOME",
+    quote: `SAFIISHANDSOME`,
     wordCount: 1
   },
   {
-    quote: "It's a cave full of idiots and it's great",
+    quote: `It's a cave full of idiots and it's <em>great</em>`,
     wordCount: 9
   },
   {
-    quote: "It's a cluster of fuckeds",
+    quote: `It's a <em>cluster</em> of fuckeds`,
     wordCount: 5
   },
   {
-    quote: "Enslave a wizard",
+    quote: `Enslave a wizard!`,
     wordCount: 3
   },
   {
-    quote: "Guess I'm gonna hit this API 40 times in a fuckin' row",
+    quote: `Guess I'm gonna hit this API 40 times in a fuckin' row`,
     wordCount: 12
   },
   {
-    quote: "This is what the professional world refers to as a clusterfuck",
+    quote: `This is what the professional world refers to as a <em>clusterfuck</em>`,
     wordCount: 11
   },
   {
-    quote: "🔥🔥🔥🔥🔥🔥🔥",
+    quote: `🔥🔥🔥🔥🔥🔥🔥`,
     wordCount: 1
   },
   {
-    quote: "I want you to turn yourself into an array",
+    quote: `I want you to turn yourself into an array`,
     wordCount: 9
   },
   {
-    quote: "The best jokes need to be explained",
+    quote: `The best jokes need to be explained`,
     wordCount: 7
   },
   {
-    quote: "it's same same",
+    quote: `it's same same`,
     wordCount: 3,
     append: true
   },
   {
-    quote: "If it's written down it must be true",
+    quote: `If it's written down it must be true`,
     wordCount: 8
   },
   {
-    quote: "Say my name, wizard",
-    wordCount: 4
+    quote: `Say my name, wizard!`,
+    wordCount: 4,
+    punctuation: false
   },
   {
-    quote: "Don't waste three days of your mortal existence on a semicolon",
+    quote: `Don't waste three days of your mortal existence on a semicolon`,
     wordCount: 11
   },
   {
-    quote: "Everything is arbitrary and will is an illusion",
+    quote: `Everything is arbitrary and will is an illusion`,
     wordCount: 8
   },
   {
-    quote: "Git will give you no feedback",
+    quote: `Git will give you no feedback`,
     wordCount: 6
   },
   {
-    quote: "You'll never know if you're doing anything right",
+    quote: `You'll never know if you're doing anything right`,
     wordCount: 8
   },
   {
-    quote: "I would follow this sagelike turtle into war",
+    quote: `I would follow this sagelike turtle into war`,
     wordCount: 8
   },
   {
-    quote: "Take a break",
-    wordCount: 3
+    quote: `Take a break!`,
+    wordCount: 3,
+    punctuation: false
   },
   {
-    quote: "because truth is an illusion",
+    quote: `because truth is an illusion`,
     wordCount: 5,
     append: true
   },
   {
-    quote: "The answer to this question doesn't matter because soon we'll all be dead",
+    quote: `The answer to this question doesn't matter because soon we'll all be dead`,
     wordCount: 13
   },
   {
-    quote: "None of this matters",
+    quote: `None of this matters`,
     wordCount: 4
   },
   {
-    quote: "The time demons weren't available this early in the schedule",
+    quote: `The time demons weren't available this early in the schedule`,
     wordCount: 10
   },
   {
-    quote: "We create structure for the illusion of control",
+    quote: `We create structure for the illusion of control`,
     wordCount: 8
   },
   {
-    quote: "SNUBONS",
+    quote: `SNUBONS`,
     wordCount: 1
   },
   {
-    quote: "Please, I have a family",
+    quote: `Please, I have a family`,
     wordCount: 5
   },
   {
-    quote: "Look how happy this dog is",
+    quote: `Look how happy this dog is!`,
+    wordCount: 6,
+    punctuation: false
+  },
+  {
+    quote: `I could watch this for days`,
     wordCount: 6
   },
   {
-    quote: "I could watch this for days",
-    wordCount: 6
-  },
-  {
-    quote: "because capitalism",
+    quote: `because capitalism!`,
     wordCount: 2,
-    append: true
+    append: true,
+    punctuation: false,
   },
   {
-    quote: "We are using a bulbasaur database",
+    quote: `We are using a bulbasaur database`,
     wordCount: 6
   },
   {
-    quote: "Quokkas are not that happy; their face just looks like that",
+    quote: `Quokkas are not that happy; their face just looks like that`,
     wordCount: 11
   },
   {
-    quote: "666",
+    quote: `666`,
     wordCount: 1
   },
   {
-    quote: "I'd still get brunch with any of you",
+    quote: `I'd still get brunch with any of you`,
     wordCount: 8
   },
   {
-    quote: "My favourite character in the Matrix was Agent Smith",
+    quote: `My favourite character in the Matrix was Agent Smith`,
     wordCount: 9
   },
   {
-    quote: "There is no more Juno, there is only AJAX calls",
+    quote: `There is no more Juno, there is only AJAX calls`,
     wordCount: 10
   },
   {
-    quote: "I've taken my boots off again; don't make it weird",
+    quote: `I've taken my boots off again; don't make it weird`,
     wordCount: 10
   },
   {
-    quote: "This is like looking in a fun-house mirror and then realizing it's just a regular mirror",
+    quote: `This is like looking in a fun-house mirror and then realizing it's just a regular mirror`,
     wordCount: 16
   },
   {
-    quote: "That only confirms my suspicion that life is meaningless as a person",
+    quote: `That only confirms my suspicion that life is meaningless as a person`,
     wordCount: 12
   },
   {
-    quote: "Until I take out the knife, I do look exactly like html",
+    quote: `Until I take out the knife, I do look exactly like html`,
     wordCount: 12
   },
   {
-    quote: "JOEY MADE ME",
+    quote: `JOEY MADE ME`,
     wordCount: 3
   },
   {
-    quote: "stab!",
+    quote: `stab!`,
     wordCount: 1,
-    append: true
+    append: true,
+    punctuation: false
   },
   {
-    quote: "Are you lonely?",
-    wordCount: 3
+    quote: `<em>Are you lonely?</em>`,
+    wordCount: 3,
+    punctuation: false
   },
   {
-    quote: "Are there secrets? Yes there are",
+    quote: `Are there secrets? <em>Yes there are</em>`,
     wordCount: 6
   },
   {
-    quote: "Just a nice, normal console log.",
+    quote: `Just a nice, normal console log`,
     wordCount: 6
   },
   {
-    quote: "I'm amazing at this! I'm inside your head!",
-    wordCount: 8
+    quote: `I'm amazing at this! I'm inside your head!`,
+    wordCount: 8,
+    punctuation: false
   },
   {
-    quote: "Angular isn't cool don't tell anyone I ever said that",
+    quote: `Angular isn't cool don't tell anyone I ever said that`,
     wordCount: 10
   },
   {
-    quote: "Real controlling toxic relationship you're in with React",
+    quote: `<em>Real</em> controlling, toxic relationship you're in with React`,
     wordCount: 8
   },
   {
-    quote: "We are what generates all reality for the user",
+    quote: `We are what generates all reality for the user`,
     wordCount: 9
   },
   {
-    quote: "Between us devs, das some javascript shit",
+    quote: `Between us devs, <em>das some javascript shit</em>`,
     wordCount: 7
   },
   {
-    quote: "it's a very emotional decision on my part",
+    quote: `it's a very emotional decision on my part`,
     wordCount: 8
   },
   {
-    quote: "We did it, everybody; we found the worst website",
+    quote: `We did it, everybody; we found the worst website`,
     wordCount: 9
   },
   {
-    quote: "endlessly tumbling, endlessly empty...",
+    quote: `<em>endlessly tumbling, endlessly empty...</em>`,
     wordCount: "4",
-    append: true
+    append: true,
+    punctuation: false
   },
   {
     quote: `There are no answers; it's what we call 'a quirk'`,
@@ -257,7 +267,8 @@ SAFIISAWESOME.quotes = [
   },
   {
     quote: `THERE IS NO REASON!`,
-    wordCount: 4
+    wordCount: 4,
+    punctuation: false
   },
   {
     quote: `Even if you know the answer to everything, you're still definitely gonna die one day`,
@@ -280,7 +291,7 @@ SAFIISAWESOME.quotes = [
     wordCount: 11
   },
   {
-    quote: `Yea, now I can see the insides of a computer`,
+    quote: `<em>Yea</em>, now I can see the insides of a computer`,
     wordCount: 10
   },
   {
@@ -288,8 +299,9 @@ SAFIISAWESOME.quotes = [
     wordCount: 9
   },
   {
-    quote: `So you're like, "I think I found the source of my sadness and depression and therapy is working!" and then you go into a therapy session and you're feeling better; then your therapist asks, "...What about your sister?" and you're like "OH NO. It's so much deeper!"`,
-    wordCount: 47
+    quote: `So you're like, "I think I found the source of my sadness and depression and therapy is working!" and then you go into a therapy session and you're feeling better; then your therapist asks, "...What about your sister?" and you're like "<em>OH NO. It's so much deeper!</em>"`,
+    wordCount: 47,
+    punctuation: false
   },
   {
     quote: `We are victims of our mortality`,
@@ -297,14 +309,15 @@ SAFIISAWESOME.quotes = [
   },
   {
     quote: `Have you ever cried and high fived someone at the same time?`,
-    wordCount: 12
+    wordCount: 12,
+    punctuation: false
   },
   {
     quote: `Age and time is relentless: a slog forward toward death`,
     wordCount: 10
   },
   {
-    quote: `Deborah says it's something I need to work on and Deborah is for sure correct`,
+    quote: `Deborah says it's something I need to work on and Deborah is <em>for sure correct</em>`,
     wordCount: 15
   },
   {
@@ -342,7 +355,8 @@ SAFIISAWESOME.quotes = [
   },
   {
     quote: `Wow! that sounds like the march of mortality!`,
-    wordCount: 8
+    wordCount: 8,
+    punctuation: false
   }
 ];
 
@@ -369,6 +383,9 @@ SAFIISAWESOME.generateIpsum = (numParagraphs, numWords) => {
         const quotesThatWillFit = SAFIISAWESOME.quotes.filter ( (value) => {
           return value.wordCount <= (numWords - allTheWords);
         })
+
+        // assign the new ipsum at the end of the for loop?
+        // figure out a check if the number is random so that we don't get an error
         const numbah = Math.floor(Math.random() * quotesThatWillFit.length);
         const newQuote = quotesThatWillFit[numbah];
         if (ipsum == "") {
